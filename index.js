f156cb97bab63838d4d2cd36595201c2256310ca
Brant Stuns <brantstuns@gmail.com>
2016-12-13 23:21:48 -0700

    Added handling for null file as well as use strict

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+'use strict';
+
 var through = require('through2');
 
 module.exports = function () {
   function transform (file, encoding, callback) {
+    if (file.isNull()) {
+      return callback(null, file);
+    }
+
     if ([/>>>/, /<<</, /===/].some((regex) => regex.test(file))) {
       console.log('These files might have some sketchy git stuff going on, ' +
         'please remove leftover merge conflict syntax!\n\n')
